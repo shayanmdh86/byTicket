4284947c38f56fe04298d4a1ffb79d62d3ee8d84
Shayan <Shayanmdhy0@gmail.com>
2024-02-16 22:37:54 +0330

    Defining a query to get passengers from the database and convert it into a deliverable DTO

---
+++ App.Infra.Repo.Ef/Passenger/PassengerQueryRepoEf.cs
@@ -0,0 +1,17 @@
+﻿using App.Infra.SqlServer.Ef.Dbctx;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Infra.Repo.Ef.Passenger
+{
+    public class PassengerQueryRepoEf
+    {
+        public PassengerQueryRepoEf(AppDbContext)
+        {
+            
+        }
+    }
+}
