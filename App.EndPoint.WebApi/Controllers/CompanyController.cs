e393bfa73f2baf2fe0b344e47c5845705c9f88f2
Shayan <Shayanmdhy0@gmail.com>
2024-12-27 20:26:55 +0330

    Implementing a company phone validation method

--- App.EndPoint.WebApi/Controllers/CompanyController.cs
+++ App.EndPoint.WebApi/Controllers/CompanyController.cs
@@ -1,5 +1,7 @@
 ﻿using App.Domain.Core.Company.AppService;
 using App.Domain.Core.Company.DTOs;
+using App.Domain.Core.Company.Service;
+using App.Domain.Service.Company;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
@@ -11,17 +13,28 @@ namespace App.EndPoint.WebApi.Controllers
     public class CompanyController : ControllerBase
     {
         private readonly ICompanyAppService _companyAppService;
+        //private readonly ICompanyService _companyService;
 
         public CompanyController(ICompanyAppService companyAppService)
         {
             _companyAppService = companyAppService;
+            //_companyService = companyService;
         }
 
         [HttpPost]
         public async Task<IActionResult>PostCompany(CompanyInputDto companyInputDto)
         {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            if (!CompanyService.IsValidPhoneNumber(companyInputDto.PhoneNumber))
+            {
+                return BadRequest("Invalid Phone Number Is Format.");
+            }
+
            await _companyAppService.CreateCompany(companyInputDto);
-          return Ok(companyInputDto);
+            return Ok(companyInputDto);
 
         }
     }
